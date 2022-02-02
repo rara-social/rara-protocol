69dea52bf9fc17dc28693c157bdd1a0d44fc4a0d
pooleja <pooleja@users.noreply.github.com>
2022-02-02 10:56:26 -0500

    Rename MakerVault to MakerRegistrar

--- test/Maker/MakerVault.ts
+++ test/Maker/MakerVault.ts
@@ -10,23 +10,23 @@ import {
   NFT_NOT_REGISTERED,
 } from "../Scripts/errors";
 
-describe("MakerVault", function () {
+describe("MakerRegistrar", function () {
   it("Should get initialized with address manager", async function () {
     const [OWNER] = await ethers.getSigners();
-    const { makerVault, addressManager } = await deploySystem(OWNER);
+    const { makerRegistrar, addressManager } = await deploySystem(OWNER);
 
     // Verify the address manager was set
-    const currentAddressManager = await makerVault.addressManager();
+    const currentAddressManager = await makerRegistrar.addressManager();
     expect(currentAddressManager).to.equal(addressManager.address);
   });
 
   it("Should verify NFT ownership on register", async function () {
     const [OWNER] = await ethers.getSigners();
-    const { makerVault, testingStandard1155 } = await deploySystem(OWNER);
+    const { makerRegistrar, testingStandard1155 } = await deploySystem(OWNER);
 
     // Since this is trying to register a non-existing NFT it should show the caller doesn't own it
     await expect(
-      makerVault.registerNFT(
+      makerRegistrar.registerNFT(
         testingStandard1155.address,
         "1",
         ZERO_ADDRESS,
@@ -37,7 +37,7 @@ describe("MakerVault", function () {
 
   it("Should allow NFT registration only once", async function () {
     const [OWNER, ALICE, BOB] = await ethers.getSigners();
-    const { makerVault, roleManager, testingStandard1155 } = await deploySystem(
+    const { makerRegistrar, roleManager, testingStandard1155 } = await deploySystem(
       OWNER
     );
 
@@ -49,13 +49,13 @@ describe("MakerVault", function () {
 
     // Register the NFT from Alice's account and put Bob as the creator
     // Verify event as well
-    await makerVault
+    await makerRegistrar
       .connect(ALICE)
       .registerNFT(testingStandard1155.address, NFT_ID, BOB.address, "0");
 
     // Verify it can't be registered again now that it is registered
     await expect(
-      makerVault
+      makerRegistrar
         .connect(ALICE)
         .registerNFT(testingStandard1155.address, NFT_ID, BOB.address, "0")
     ).to.revertedWith(ALREADY_REGISTERED);
@@ -63,7 +63,7 @@ describe("MakerVault", function () {
 
   it("Should emit registration event and verify mappings", async function () {
     const [OWNER, ALICE, BOB] = await ethers.getSigners();
-    const { makerVault, roleManager, testingStandard1155 } = await deploySystem(
+    const { makerRegistrar, roleManager, testingStandard1155 } = await deploySystem(
       OWNER
     );
 
@@ -87,7 +87,7 @@ describe("MakerVault", function () {
     // Register the NFT from Alice's account and put Bob as the creator
     // Verify event as well
     await expect(
-      makerVault
+      makerRegistrar
         .connect(ALICE)
         .registerNFT(
           testingStandard1155.address,
@@ -96,7 +96,7 @@ describe("MakerVault", function () {
           OPTION_BITS
         )
     )
-      .to.emit(makerVault, "Registered")
+      .to.emit(makerRegistrar, "Registered")
       .withArgs(
         testingStandard1155.address,
         BigNumber.from(NFT_ID),
@@ -110,16 +110,16 @@ describe("MakerVault", function () {
     // Verify lookups are set in the mapping
     // Verify source id from nft param
     expect(
-      await makerVault.nftToSourceLookup(testingStandard1155.address, NFT_ID)
+      await makerRegistrar.nftToSourceLookup(testingStandard1155.address, NFT_ID)
     ).to.equal(EXPECTED_SOURCE_ID);
 
     // Verify source from meta id
-    expect(await makerVault.metaToSourceLookup(derivedMetaId)).to.equal(
+    expect(await makerRegistrar.metaToSourceLookup(derivedMetaId)).to.equal(
       EXPECTED_SOURCE_ID
     );
 
     // Verify registration details from source id
-    const [registered, owner, creator] = await makerVault.sourceToDetailsLookup(
+    const [registered, owner, creator] = await makerRegistrar.sourceToDetailsLookup(
       BigNumber.from(EXPECTED_SOURCE_ID)
     );
     expect(registered).to.equal(true);
@@ -129,17 +129,17 @@ describe("MakerVault", function () {
 
   it("Should verify NFT ownership on deregister", async function () {
     const [OWNER] = await ethers.getSigners();
-    const { makerVault, testingStandard1155 } = await deploySystem(OWNER);
+    const { makerRegistrar, testingStandard1155 } = await deploySystem(OWNER);
 
     // Since this is trying to deregister a non-existing NFT it should show the caller doesn't own it
     await expect(
-      makerVault.deregisterNFT(testingStandard1155.address, "1")
+      makerRegistrar.deregisterNFT(testingStandard1155.address, "1")
     ).to.revertedWith(NFT_NOT_OWNED);
   });
 
   it("Should register and deregister and check event", async function () {
     const [OWNER, ALICE, BOB] = await ethers.getSigners();
-    const { makerVault, testingStandard1155, roleManager } = await deploySystem(
+    const { makerRegistrar, testingStandard1155, roleManager } = await deploySystem(
       OWNER
     );
 
@@ -151,13 +151,13 @@ describe("MakerVault", function () {
 
     // Verify NFT unknown in the system can't be deregsitered
     await expect(
-      makerVault
+      makerRegistrar
         .connect(ALICE)
         .deregisterNFT(testingStandard1155.address, NFT_ID)
     ).to.revertedWith(NFT_NOT_FOUND);
 
     // Register it
-    await makerVault
+    await makerRegistrar
       .connect(ALICE)
       .registerNFT(testingStandard1155.address, NFT_ID, BOB.address, "0");
 
@@ -166,11 +166,11 @@ describe("MakerVault", function () {
 
     // Deregister it and check event params
     await expect(
-      makerVault
+      makerRegistrar
         .connect(ALICE)
         .deregisterNFT(testingStandard1155.address, NFT_ID)
     )
-      .to.emit(makerVault, "Deregistered")
+      .to.emit(makerRegistrar, "Deregistered")
       .withArgs(
         testingStandard1155.address,
         BigNumber.from(NFT_ID),
@@ -180,13 +180,13 @@ describe("MakerVault", function () {
 
     // Second Deregister should fail
     await expect(
-      makerVault
+      makerRegistrar
         .connect(ALICE)
         .deregisterNFT(testingStandard1155.address, NFT_ID)
     ).to.revertedWith(NFT_NOT_REGISTERED);
 
     // Verify the flag was set to false
-    const [registered] = await makerVault.sourceToDetailsLookup(
+    const [registered] = await makerRegistrar.sourceToDetailsLookup(
       BigNumber.from(EXPECTED_SOURCE_ID)
     );
     expect(registered).to.equal(false);
