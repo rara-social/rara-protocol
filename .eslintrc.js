492c07e82d1a71370d5e318cef4020226041f741
Matt Lovan <matt@moneymail.ai>
2022-02-23 09:20:42 -0500

    deploy & docker

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,22 +3,40 @@ module.exports = {
     browser: false,
     es2021: true,
     mocha: true,
-    node: true,
+    node: true
   },
-  plugins: ["@typescript-eslint"],
+  plugins: ['@typescript-eslint'],
   extends: [
-    "standard",
-    "plugin:prettier/recommended",
-    "plugin:node/recommended",
+    'standard',
+    // 'plugin:prettier/recommended',
+    'plugin:node/recommended'
   ],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 12,
+    ecmaVersion: 12
   },
   rules: {
-    "node/no-unsupported-features/es-syntax": [
-      "error",
-      { ignores: ["modules"] },
+    'node/no-unsupported-features/es-syntax': [
+      'error',
+      { ignores: ['modules'] }
     ],
-  },
+    'node/no-missing-import': [
+      'error',
+      {
+        allowModules: [],
+        resolvePaths: ['/path/to/a/modules/directory'],
+        tryExtensions: ['.js', '.json', '.node', '.ts']
+      }
+    ],
+    'node/no-unpublished-import': [
+      'off',
+      {
+        allowModules: [],
+        convertPath: null,
+        tryExtensions: ['.js', '.json', '.node', '.ts']
+      }
+    ],
+    'comma-dangle': ['error', 'ignore'],
+    semi: ['error', 'always']
+  }
 };
