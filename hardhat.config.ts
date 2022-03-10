86de1364d510ddf8165ca14c9dc8b63645836935
Matt Lovan <matt@moneymail.ai>
2022-03-10 09:56:05 -0500

    add localhost config for metamask

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,6 @@
-import * as dotenv from "dotenv";
+import * as dotenv from 'dotenv';
 
-import { HardhatUserConfig, task } from 'hardhat/config';
+import {HardhatUserConfig, task} from 'hardhat/config';
 import '@nomiclabs/hardhat-etherscan';
 import '@nomiclabs/hardhat-waffle';
 import '@typechain/hardhat';
@@ -14,7 +14,7 @@ dotenv.config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
 
   for (const account of accounts) {
@@ -33,18 +33,22 @@ const config: HardhatUserConfig = {
   //     process.env.COINMARKETCAP || 'e27b406d-691c-49cd-9e63-c40befea0f69'
   // },
   networks: {
-    ganache: {
-      url: "http://127.0.0.1:8545"
-    }
+    hardhat: {
+      chainId: 1337,
+    },
+    localhost: {
+      url: 'http://127.0.0.1:8545',
+    },
   },
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY,
   },
-  namedAccounts: { // For deployment scripts under /deploy
+  namedAccounts: {
+    // For deployment scripts under /deploy
     deployer: {
       default: 0, // take the first account as default from the network config
       // This account will deploy contracts and be set as protocol admin and also admin for all proxies
-    }
+    },
   },
 };
 
