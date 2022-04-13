33d64aee32de6db1613664e4ae2bee274ff7bf81
Matt Lovan <matt@moneymail.ai>
2022-04-13 10:48:28 -0400

    add note to script 3

--- subgraph/scripts/functions/3_spendReaction.js
+++ subgraph/scripts/functions/3_spendReaction.js
@@ -13,8 +13,10 @@ const takerNftId = "44";
 const reactionId =
   "0x341c6ffe3493b3337dcde53c58aec123f85799a68d0dc4d2e46424f90bc3db0d";
 const reactionQuantity = 10;
+
+// neither of theses work: '{"jsonrpc":"2.0","id":49,"error":{"code":-32000,"message":"execution reverted"}}'
 // const ipfsMetadataHash = 0;
-const ipfsMetadataHash = "QmV288zHttJJwPBZAW3L922dBypWqukFNWzekT6chxW4Cu";
+// const ipfsMetadataHash = "QmV288zHttJJwPBZAW3L922dBypWqukFNWzekT6chxW4Cu";
 
 async function main() {
   const reactor = await getWallet("reactor");
