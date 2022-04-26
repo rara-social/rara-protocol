b9c3b34f2c52b094455a33300bec70dc6323da8a
pooleja <pooleja@users.noreply.github.com>
2022-04-26 14:00:07 -0400

    Remove unneeded requires

--- contracts/SigmoidCuratorVault/Curve/ExtendedMath.sol
+++ contracts/SigmoidCuratorVault/Curve/ExtendedMath.sol
@@ -1,6 +1,8 @@
 //SPDX-License-Identifier: GPL-3.0-only
 pragma solidity 0.8.9;
 
+/// @dev ^2, ^3, and sqrt functions
+/// Solidity 0.8+ should catch any overflows on multiplications
 library ExtendedMath {
     /**
      * @return The given number raised to the power of 2
@@ -9,21 +11,14 @@ library ExtendedMath {
         if (a == 0) {
             return 0;
         }
-        int256 c = a * a;
-        require(c / a == a, "ExtendedMath: squaring overflow");
-        return c;
+        return a * a;
     }
 
     function pow3(int256 a) internal pure returns (int256) {
         if (a == 0) {
             return 0;
         }
-        int256 c = a * a;
-        require(c / a == a, "ExtendedMath: cubing overflow2");
-
-        int256 d = c * a;
-        require(d / a == c, "ExtendedMath: cubing overflow3");
-        return d;
+        return a * a * a;
     }
 
     /**
