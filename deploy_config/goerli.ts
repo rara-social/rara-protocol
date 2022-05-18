6e71ff566ce2c9f6c62fb6baac93257002afac94
pooleja <pooleja@users.noreply.github.com>
2022-05-18 14:36:15 -0400

    Curator share nft URI'

--- deploy_config/goerli.ts
+++ deploy_config/goerli.ts
@@ -5,6 +5,7 @@ const config: DeployConfig = {
   bondingCurveB: "",
   bondingCurveC: "",
   curatorLiabilityBasisPoints: "",
+  curatorTokenNftUri: "",
   fxChildBridgeAddress: "",
   fxRootBridgeAddress: "0x3d1d3E34f7fB6D26245E6640E1c50710eFFf15bA",
   fxRootCheckPointManager: "0x2890bA17EfE978480615e330ecB65333b880928e",
--- deploy_config/polygon.ts
+++ deploy_config/polygon.ts
@@ -1,20 +1,21 @@
 import DeployConfig from "./types";
 
 const config: DeployConfig = {
-  reactionNftUri: "https://rara.social/polygon-nfts/{id}.json",
-  fxChildBridgeAddress: "0x8397259c983751DAf40400790063935a11afa28a", // https://docs.polygon.technology/docs/develop/l1-l2-communication/state-transfer/
-  reactionPrice: "1000000", // Base units of payment token with 6 decimals
-  curatorLiabilityBasisPoints: "9000", // 90% goes to curator liability
-  saleReferrerBasisPoints: "100",// 1% goes to the referrer on reaction sale
-  spendTakerBasisPoints: "700", // 7% of curator liability goes to the taker
-  spendReferrerBasisPoints: "100", // 1% of curator liability goes to the referrer
-  paymentTokenAddress: "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174", // USDC on polygon
   bondingCurveA: "5000", // 0.5 * 1_000_000 => max price / 2 => 1 is max price of curve
   bondingCurveB: "10000000", // midpoint is 10m tokens sold
   bondingCurveC: "29000000000000", // Super flat curve - this is steepness param
+  curatorLiabilityBasisPoints: "9000", // 90% goes to curator liability
+  curatorTokenNftUri: "https://protocol-api.rara.social/internal/curatortoken/{id}",
+  fxChildBridgeAddress: "0x8397259c983751DAf40400790063935a11afa28a", // https://docs.polygon.technology/docs/develop/l1-l2-communication/state-transfer/
   fxRootBridgeAddress: "", // Not set for L2
   fxRootCheckPointManager: "", // Not set for L2
+  paymentTokenAddress: "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174", // USDC on polygon
+  reactionNftUri: "https://protocol-api.rara.social/internal/reaction/{id}",
+  reactionPrice: "1000000", // Base units of payment token with 6 decimals
   royaltyRegistry: "0x28EdFcF0Be7E86b07493466e7631a213bDe8eEF2", // deployed royalty registry https://royaltyregistry.xyz/lookup
+  saleReferrerBasisPoints: "100",// 1% goes to the referrer on reaction sale
+  spendReferrerBasisPoints: "100", // 1% of curator liability goes to the referrer
+  spendTakerBasisPoints: "700", // 7% of curator liability goes to the taker
 }
 
 export default config;
