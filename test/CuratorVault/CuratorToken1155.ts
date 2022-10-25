8933d52cc3986161b42176a6291210536d0200ae
Matt Lovan <matt@rara.social>
2022-10-25 14:19:50 -0400

    add tests for contractURI

--- test/CuratorVault/CuratorToken1155.ts
+++ test/CuratorVault/CuratorToken1155.ts
@@ -1,9 +1,10 @@
 import {expect} from "chai";
 import {ethers} from "hardhat";
 import {deploySystem} from "../Scripts/setup";
+import {TEST_CONTRACT_URI} from "../Scripts/constants";
 import {NOT_ADMIN} from "../Scripts/errors";
 
-describe("CuratorToken", function () {
+describe("CuratorTokens", function () {
   it("Should get initialized with address manager", async function () {
     const [OWNER] = await ethers.getSigners();
     const {curatorToken, addressManager} = await deploySystem(OWNER);
@@ -38,5 +39,28 @@ describe("CuratorToken", function () {
 
     // Should succeed
     await curatorToken.burn(ALICE.address, "1", "1");
+  });
+
+  it("Should set contractURI if authorized", async function () {
+    const [OWNER, BOB] = await ethers.getSigners();
+    const {curatorToken, roleManager} = await deploySystem(OWNER);
+
+    // check for contractURI
+    let contractURI = await curatorToken.contractUri();
+    expect(contractURI).to.equal(TEST_CONTRACT_URI);
+
+    // try and set contract URI (unauthorized)
+    await expect(
+      curatorToken.connect(BOB).setContractUri("should error")
+    ).to.be.revertedWith(NOT_ADMIN);
+
+    // Grant authorization to Bob
+    const reactionMinterRole = await roleManager.CURATOR_TOKEN_ADMIN();
+    await roleManager.grantRole(reactionMinterRole, BOB.address);
+
+    // try and set contract URI (unauth)
+    await curatorToken.connect(BOB).setContractUri("test string");
+    contractURI = await curatorToken.contractUri();
+    expect(contractURI).to.equal("test string");
   });
 });
