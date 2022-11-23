c580215e3f08e889ab2ac24532948bf30dc8f9d8
Matt Lovan <matt@rara.social>
2022-11-23 11:48:46 -0500

    add graph - like token

---
+++ subgraph/src/mapping/LikeTokenFactory.ts
@@ -0,0 +1,31 @@
+import {log} from "@graphprotocol/graph-ts";
+
+import {LikeTokenContract} from "../../generated/schema";
+
+import {TokenDeployed} from "../../generated/LikeTokenFactory/LikeTokenFactory";
+
+export function handleTokenDeployed(event: TokenDeployed): void {
+  log.log(3, "TokenDeployed");
+  // uint256 takerNftChainId,
+  // address takerNftAddress,
+  // uint256 takerNftId,
+  // address deployedContract
+
+  //
+  // LikeTokenContract
+  //
+  // load LikeTokenContract
+  let LikeTokenContractKey = event.params.deployedContract.toString();
+  let likeTokenContract = LikeTokenContract.load(LikeTokenContractKey);
+  if (likeTokenContract == null) {
+    likeTokenContract = new LikeTokenContract(LikeTokenContractKey);
+    likeTokenContract.takerNftChainId = event.params.takerNftChainId;
+    likeTokenContract.takerNftAddress = event.params.takerNftAddress;
+    likeTokenContract.takerNftId = event.params.takerNftId;
+    likeTokenContract.createdAt = event.block.timestamp;
+  }
+
+  likeTokenContract.updatedAt = event.block.timestamp;
+  likeTokenContract.blockNumber = event.block.number;
+  likeTokenContract.save();
+}
