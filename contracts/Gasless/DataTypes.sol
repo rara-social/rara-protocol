3a5e343f7f95db2cb4afcd7904001c43867d50e3
youfoundron <rongierlach@gmail.com>
2023-01-26 11:31:13 -0800

    write structs required for gasless sig args

---
+++ contracts/Gasless/DataTypes.sol
@@ -0,0 +1,45 @@
+//SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.9;
+
+/// @title DataTypes
+/// @dev A standard library of data types used throughout the Rara Protocol
+library DataTypes {
+    /// @notice A struct containing the necessary information to reconstruct an EIP-712 typed data signature.
+    /// @param v The signature's recovery parameter.
+    /// @param r The signature's r parameter.
+    /// @param s The signature's s parameter
+    /// @param deadline The signature's deadline
+    struct EIP712Signature {
+        uint8 v;
+        bytes32 r;
+        bytes32 s;
+        uint256 deadline;
+    }
+
+    /// @notice A struct containing the parameters required for the `reactWithSig()` function.
+    ///         Parameters are the same as the regular `react()` function, with the reactor's (signer) address and an EIP712Signature added.
+    /// @param reactor The reactor which is the message signer.
+    /// @param transformId Internal id used to derive the reaction token id.
+    /// @param quantity How many reactions to spend.
+    /// @param referrer Optional param to specify an address where referrer rewards are allocated
+    /// @param optionBits Optional param to specify options how the user wants transform reaction
+    /// @param takerNftChainId Chain ID where the NFT lives
+    /// @param takerNftAddress Target contract where the reaction is targeting
+    /// @param takerNftId Target NFT ID in the contract
+    /// @param curatorVaultOverride Optional address of non-default curator vault
+    /// @param ipfsMetadataHash Optional hash of any metadata being associated with spend action
+    /// @param sig The EIP712Signature struct containing the follower's signature.
+    struct ReactWithSigData {
+        address reactor,
+        uint256 transformId,
+        uint256 quantity,
+        address referrer,
+        uint256 optionBits,
+        uint256 takerNftChainId,
+        address takerNftAddress,
+        uint256 takerNftId,
+        address curatorVaultOverride,
+        string memory ipfsMetadataHash
+        EIP712Signature sig;
+    }
+}
