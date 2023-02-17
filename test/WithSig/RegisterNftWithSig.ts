f9a35268fdee9c488266a04c5acdf54c6227d56d
youfoundron <rongierlach@gmail.com>
2023-02-17 08:47:07 -0800

    write better test for RegisterNftWithSig

--- test/WithSig/RegisterNftWithSig.ts
+++ test/WithSig/RegisterNftWithSig.ts
@@ -4,25 +4,40 @@ import {MAX_UINT256} from "../Scripts/constants";
 import {deploySystem, TEST_SALE_CREATOR_BP} from "../Scripts/setup";
 import {NFT_NOT_OWNED} from "../Scripts/errors";
 import {getRegisterNftWithSigParts} from "../helpers/utils";
+import {BigNumber} from "ethers";
+import {deriveTransformId} from "../Scripts/derivedParams";
 
 describe("MakerRegistrar RegisterNftWithSig", function () {
-  it("Should allow 721 NFT registration ", async function () {
+  it("Should emit registration event and verify mappings", async function () {
     const [OWNER, ALICE, BOB] = await ethers.getSigners();
-    const {makerRegistrar, testingStandard721, parameterManager} =
+    const {makerRegistrar, testingStandard1155, parameterManager} =
       await deploySystem(OWNER);
 
     // Sig retrieval vars
     const verifyingContract = makerRegistrar.address;
     const signer = ALICE;
     // Args for registerNftWithSig
-    // Args for freeReactWithSig
+    const chainId = (await ethers.provider.getNetwork()).chainId;
     const registrant = ALICE.address;
-    const nftContractAddress = testingStandard721.address;
+    const nftContractAddress = testingStandard1155.address;
     const nftId = "1";
     const creatorAddress = BOB.address;
     const creatorSaleBasisPoints = TEST_SALE_CREATOR_BP;
     const optionBits = "0";
     const ipfsMetadataHash = "QmV288zHttJJwPBZAW3L922dBypWqukFNWzekT6chxW4Cu";
+
+    // Get the source ID from the lookup
+    const EXPECTED_SOURCE_ID = await makerRegistrar.deriveSourceId(
+      chainId,
+      nftContractAddress,
+      nftId
+    );
+
+    // Encode the params and hash it to get the meta URI
+    const derivedMetaId = deriveTransformId(
+      EXPECTED_SOURCE_ID,
+      BigNumber.from(0)
+    );
 
     // Sig validation vars
     const nonce = (await parameterManager.sigNonces(signer.address)).toNumber();
@@ -61,24 +76,39 @@ describe("MakerRegistrar RegisterNftWithSig", function () {
       })
     ).to.revertedWith(NFT_NOT_OWNED);
 
-    // Mint the NFT to Alice
-    testingStandard721.mint(ALICE.address, nftId);
+    // Mint an NFT to Alice
+    testingStandard1155.mint(ALICE.address, nftId, "1", [0]);
 
     // Register the NFT from Alice's account and put Bob as the creator
-    await makerRegistrar.connect(ALICE).registerNftWithSig({
-      registrant,
-      nftContractAddress,
-      nftId,
-      creatorAddress,
-      creatorSaleBasisPoints,
-      optionBits,
-      ipfsMetadataHash,
-      sig: {
-        v: signature.v,
-        r: signature.r,
-        s: signature.s,
-        deadline: MAX_UINT256,
-      },
-    });
+    await expect(
+      makerRegistrar.connect(ALICE).registerNftWithSig({
+        registrant,
+        nftContractAddress,
+        nftId,
+        creatorAddress,
+        creatorSaleBasisPoints,
+        optionBits,
+        ipfsMetadataHash,
+        sig: {
+          v: signature.v,
+          r: signature.r,
+          s: signature.s,
+          deadline: MAX_UINT256,
+        },
+      })
+    )
+      .to.emit(makerRegistrar, "Registered")
+      .withArgs(
+        chainId,
+        nftContractAddress,
+        BigNumber.from(nftId),
+        ALICE.address,
+        [BOB.address],
+        [BigNumber.from(TEST_SALE_CREATOR_BP)],
+        BigNumber.from(optionBits),
+        BigNumber.from(EXPECTED_SOURCE_ID),
+        derivedMetaId,
+        ipfsMetadataHash
+      );
   });
 });
